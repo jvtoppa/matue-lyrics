c7cd238686d3be7a08ecf96f034a6bd918ffee15
jvtoppa <53586039+jvtoppa@users.noreply.github.com>
2022-08-09 20:22:12 -0300

    Update tuezin.py

--- tuezin.py
+++ tuezin.py
@@ -10,7 +10,7 @@ auth = tweepy.OAuth1UserHandler(creds.oauth1, creds.oauth2, creds.oauth3, creds.
 api = tweepy.API(auth)
 while True:
     #abre letras
-    with open("C:\\Users\\T-Gamer\\Desktop\\área de trabalho\\botpy\\tue_musicas.txt", 'r', encoding="utf-8") as file:
+    with open("tue_musicas.txt", 'r', encoding="utf-8") as file:
     #le as linhas e seleciona uma linha aleatoriamente
         x = len(file.readlines())
 
